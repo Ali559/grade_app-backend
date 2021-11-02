c112a54c343113b9fa9a8767da139f04b384602d
Ali Barznji <alibarznji@Alis-MacBook-Pro.local>
2021-11-02 15:36:53 +0300

    testing heroku server

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -4,8 +4,30 @@ const controller = {
 		const { email, password } = req.body;
 		res.json({ email, password });
 	},
-	signup: (req, res) => {},
-	getAll: (req, res) => {}
+	signup: (req, res) => {
+		const { username, email, password } = req.body;
+		const sgMail = require('@sendgrid/mail');
+		sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+		const msg = {
+			to: 'atech9801@gmail.com', // Change to your recipient
+			from: 'ali99yasin@gmail.com', // Change to your verified sender
+			subject: 'Account Verification',
+			text: `Hello ${username}, please verify your email address from the link below`,
+			html: `    <h1>Verification Link:</h1><br>
+    <a href="http://grade-app.herokuapp.com"></a>`
+		};
+		sgMail
+			.send(msg)
+			.then(() => {
+				console.log('Email sent');
+			})
+			.catch((error) => {
+				console.error(error);
+			});
+	},
+	getAll: (req, res) => {
+		res.send('<h1>Hello there</h1>');
+	}
 };
 
 module.exports = controller;
